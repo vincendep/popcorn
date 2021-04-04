c89d0375e20ddbc1189ef1494a83f39397d32b80
Vincenzo De Petris <vincenzodepetris@gmail.it>
2021-04-04 22:29:58 +0200

    Add movie credits detail page

---
+++ src/app/pages/movie-credits-detail/movie-credits-detail.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { TmdbService } from 'src/app/services/tmdb.service';
+
+@Component({
+  selector: 'movie-credits-detail',
+  templateUrl: './movie-credits-detail.page.html',
+  styleUrls: ['./movie-credits-detail.page.scss'],
+})
+export class MovieCreditsDetailPage implements OnInit {
+
+  @Input('directors') directors
+  @Input('cast') cast
+
+  constructor(
+    private modalController: ModalController,
+    private tmdb: TmdbService
+  ) {}
+
+  ngOnInit() {}
+
+  getImageUrl(path: string) {
+    return this.tmdb.getImageUrl(path)
+  }
+
+  dismiss() {
+    this.modalController.dismiss();
+  }
+}
