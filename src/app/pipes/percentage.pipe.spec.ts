0d6441b5fc088ddf84361b2f2ec1a5c64e452424
Vincenzo De Petris <vincenzodepetris@gmail.it>
2021-04-05 19:56:23 +0200

    Remove duplicate pipe

--- src/app/pipes/percentage.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PercentagePipe } from './percentage.pipe';
-
-describe('PercentagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PercentagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
