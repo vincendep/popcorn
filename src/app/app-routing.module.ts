7a635ab12f38a51149ac7587566c557f02493998
Vincenzo De Petris <vincenzodepetris@gmail.it>
2021-04-05 19:57:30 +0200

    Enable tracing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 const routes: Routes = [
   {
@@ -13,7 +14,12 @@ const routes: Routes = [
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(
+      routes,
+      {
+        preloadingStrategy: PreloadAllModules,
+        enableTracing: ! environment.production
+      })
   ],
   exports: [RouterModule]
 })
