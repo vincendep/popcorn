05c6b138fd6ab187a87c72a301e53320eabf349d
Vincenzo De Petris <vincenzodepetris@gmail.it>
2021-04-06 20:38:45 +0200

    Update tmdb service

--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -10,13 +12,24 @@ export class TmdbService {
   private apiKey = environment.TMDB_API_KEY
   private baseUrl = "https://api.themoviedb.org/3"
   private imageBaseUrl = "https://image.tmdb.org/t/p"
-  private defaultExternalSource = "imdb_id"
 
   constructor(
     private http: HttpClient
   ) { }
 
-  find(id: string, externalSource: string = this.defaultExternalSource) {
+  discoverMovie(filter: {genres?: string, keywords?: string, watchProviders?: string} ) {
+    const path = 'discover/movie'
+    return this.http.get(this.buildUrl(path), {
+      params: {
+        api_key: this.apiKey,
+        with_genres: filter.genres,
+        with_keywords: filter.keywords,
+        with_watch_providers: filter.watchProviders
+      }
+    })
+  }
+
+  find(id: string, externalSource: string = "imdb_id") {
     const path = `find/${id}`
     return this.http.get(this.buildUrl(path), {
       params: {
@@ -42,6 +55,24 @@ export class TmdbService {
         api_key: this.apiKey
       }
     })
+  }
+
+  private movieGenresCache = {}
+
+  getMovieGenresList(language: string = 'en-US') {
+    if (this.movieGenresCache[language]) {
+      return of(this.movieGenresCache[language])
+    } else {
+      const path = 'genre/movie/list'
+      return this.http.get(this.buildUrl(path), {
+        params: {
+          api_key: this.apiKey,
+          language: language
+        }
+      }).pipe(
+        tap(genresList => this.movieGenresCache[language] = genresList)
+      )
+    }
   }
 
   getMovieRecommendations(id: Number, page: Number = 1) {
