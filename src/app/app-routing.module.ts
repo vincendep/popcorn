67016e715faf1633e0c5513a5e2470a91cb0e563
Vincenzo De Petris <vincenzodepetris@gmail.it>
2021-04-10 11:57:19 +0200

    Disable router tracing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
       routes,
       {
         preloadingStrategy: PreloadAllModules,
-        enableTracing: ! environment.production
+        enableTracing: false
       })
   ],
   exports: [RouterModule]
