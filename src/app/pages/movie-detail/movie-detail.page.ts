3e3c84f7f3b1ac9bce21d8b0a59dd19d06569ba8
Vincenzo De Petris <vincenzodepetris@gmail.it>
2021-04-21 22:01:33 +0200

    Add disliked list and fix bug in library service

--- src/app/pages/movie-detail/movie-detail.page.ts
+++ src/app/pages/movie-detail/movie-detail.page.ts
@@ -1,31 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ModalController } from '@ionic/angular';
+import { AlertController, ModalController } from '@ionic/angular';
 import { TmdbMovieService } from 'src/app/services/tmdb/tmdb-movie.service';
-import { Observable, Subscription } from 'rxjs';
-import { tap, map, filter } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of, Subject, Subscription } from 'rxjs';
+import { tap, map, filter, take, takeUntil } from 'rxjs/operators';
 import { MovieCreditsDetailPage } from '../movie-credits-detail/movie-credits-detail.page';
 import { Movie } from 'src/app/models/domain/movie';
 import { Page } from 'src/app/models/page';
 import { MovieStreamingProviders, StreamingProvider } from 'src/app/models/domain/streaming-provider';
 import { TmdbStreamingProviderService } from 'src/app/services/tmdb/tmdb-streaming-provider.service';
 import { LibraryService } from 'src/app/services/library.service';
+import { List } from 'src/app/models/domain/list';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.page.html',
   styleUrls: ['./movie-detail.page.scss'],
 })
-export class MovieDetailPage implements OnInit {
+export class MovieDetailPage implements OnInit, OnDestroy {
 
   private movie$: Observable<Movie>
+  private isLiked: boolean;
+  private isDisliked: boolean;
+  private isInWatchList: boolean;
   private streamingProviders$: Observable<MovieStreamingProviders>
   private recommendedMovies$: Observable<Page<Movie>>
   private movieCredits$
   private directors
   private cast
+  private done$ = new Subject();
 
   constructor(
+    private alertController: AlertController,
     private route: ActivatedRoute,
     private modalController: ModalController,
     private movieService: TmdbMovieService,
@@ -34,15 +40,54 @@ export class MovieDetailPage implements OnInit {
   ) { }
 
   ngOnInit() {
-    const movieId = +this.route.snapshot.paramMap.get("id");
-    this.movie$ = this.movieService.getMovieDetails(movieId)
-    this.recommendedMovies$ = this.movieService.getMovieRecommendations(movieId)
-    this.streamingProviders$ = this.watchProviderService.getMovieWatchProviders(movieId)
-    this.movieService.getMovieCredits(movieId)
+    const movieId = this.route.snapshot.paramMap.get("id");
+    this.movie$ = this.movieService.getMovieDetails(+movieId)
+    this.recommendedMovies$ = this.movieService.getMovieRecommendations(+movieId)
+    this.streamingProviders$ = this.watchProviderService.getMovieWatchProviders(+movieId)
+    this.movieService.getMovieCredits(+movieId)
       .pipe(
+        takeUntil(this.done$),
         tap((credits: any) => this.cast = credits.cast.slice(0, 25)),
         tap((credits: any) => this.directors = credits.crew.filter(person => person.job === "Director")))
       .subscribe()
+    this.libraryService.isLiked(movieId)
+      .pipe(takeUntil(this.done$))
+      .subscribe(isLiked => this.isLiked = isLiked)
+    this.libraryService.isDisliked(movieId)
+      .pipe(takeUntil(this.done$))
+      .subscribe(isDisliked => this.isDisliked = isDisliked)
+    this.libraryService.isInWatchList(movieId)
+        .pipe(takeUntil(this.done$))
+        .subscribe(isInWatchList => this.isInWatchList = isInWatchList)
+  }
+
+  ngOnDestroy() {
+    this.done$.next(true)
+    this.done$.complete();
+  }
+
+  toggleLike(movie: Movie) {
+    if (this.isLiked) {
+      this.libraryService.removeFromLiked(movie.id)
+    } else {
+      this.libraryService.addToLiked(movie)
+    }
+  }
+
+  toggleDislike(movie: Movie) {
+    if (this.isDisliked) {
+      this.libraryService.removeFromDisliked(movie.id)
+    } else {
+      this.libraryService.addToDisliked(movie)
+    }
+  }
+
+  toggleWatchList(movie: Movie) {
+    if (this.isInWatchList) {
+      this.libraryService.removeFromWatchList(movie.id)
+    } else {
+      this.libraryService.addToWatchList(movie)
+    }
   }
 
   async showMovieCredits() {
@@ -56,15 +101,45 @@ export class MovieDetailPage implements OnInit {
     return await modal.present();
   }
 
-  isInWatchList(movie: Movie) {
-    return this.libraryService.isInWatchList(movie)
-  }
-
-  addToWatchList(movie: Movie) {
-    this.libraryService.addToWatchList(movie)
-  }
-
-  removeFromWatchList(movie: Movie) {
-    this.libraryService.removeFromWatchList(movie.id)
+  async showNewListAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Create a new list!',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'List name'
+        },
+        {
+          name: 'description',
+          type: 'text',
+          placeholder: 'List description'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            alert.dismiss()
+          }
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            alert.dismiss({
+              id: null,
+              name: data.name,
+              description:
+              data.description,
+              movies: []
+            })
+          }
+        }
+      ]
+    });
+    alert.present();
+    return await alert.onDidDismiss<List>().then(event => event.data)
   }
 }
--- src/app/services/library.service.ts
+++ src/app/services/library.service.ts
@@ -11,30 +11,32 @@ import { StorageService } from './storage.service';
 export class LibraryService {
   private _lists: BehaviorSubject<List[]> = new BehaviorSubject([])
   private _liked: BehaviorSubject<Movie[]> = new BehaviorSubject([])
+  private _disliked: BehaviorSubject<Movie[]> = new BehaviorSubject([])
   private _watchList: BehaviorSubject<Movie[]> = new BehaviorSubject([])
 
   public readonly lists$: Observable<List[]> = this._lists.asObservable()
   public readonly liked$: Observable<Movie[]> = this._liked.asObservable()
+  public readonly disliked$: Observable<Movie[]> = this._disliked.asObservable()
   public readonly  watchList$: Observable<Movie[]> = this._watchList.asObservable()
 
   constructor(private storageService: StorageService) {
     this.init()
   }
 
-  createList(aList: List): Observable<List> {
+  async createList(aList: List) {
     const lists = this._lists.getValue()
     const list: List = Object.assign({}, aList)
     list.id = ((Math.max(...lists.map(l => +l.id)) || 0) + 1).toString()
     lists.push(list)
-    return from(this.storageService.set("library.lists", lists)
+    return this.storageService.set("library.lists", lists)
       .then(_ => this._lists.next(lists))
-      .then(_ => list)) as Observable<List>
+      .then(_ => list)
 
   }
 
   deleteList(listId: string) {
     const lists = this._lists.getValue()
-    const list = lists.find(list => list.id === listId)
+    const list = lists.find(list => list.id == listId)
     if (list) {
       lists.splice(lists.indexOf(list), 1)
       this.storageService.set("library.lists", lists)
@@ -46,9 +48,9 @@ export class LibraryService {
 
   addToList(movie: Movie, listId: string) {
     const lists = this._lists.getValue()
-    const list = lists.find(l => l.id === listId)
+    const list = lists.find(l => l.id == listId)
     if (list) {
-      if (!list.movies.find(m => m.id === movie.id)) {
+      if (!list.movies.find(m => m.id == movie.id)) {
         list.movies.push(Movie.from(movie))
         this.storageService.set('library.lists', lists)
           .then(_ => this._lists.next(lists))
@@ -62,8 +64,8 @@ export class LibraryService {
 
   removeFromList(movieId: string, listId: string) {
     const lists = this._lists.getValue()
-    const list = lists.find(l => l.id === listId)
-    const movie = list.movies.find(m => m.id === movieId)
+    const list = lists.find(l => l.id == listId)
+    const movie = list.movies.find(m => m.id == movieId)
     if (movie) {
       list.movies.splice(list.movies.indexOf(movie), 1)
       this.storageService.set('library.lists', lists)
@@ -77,47 +79,66 @@ export class LibraryService {
     return this._lists
       .asObservable()
       .pipe(
-        map(lists => lists.filter(list => list.movies.some(m => m.id === movie.id)))
+        map(lists => lists.filter(list => list.movies.some(m => m.id == movie.id)))
       )
   }
 
-  addToWatchList(movie: Movie) {
+  async addToWatchList(movie: Movie) {
     const watchlist = this._watchList.getValue()
-    if (!watchlist.find(m => m.id === movie.id)) {
+    if (!watchlist.find(m => m.id == movie.id)) {
       watchlist.push(Movie.from(movie))
-      this.storageService.set('library.watchlist', watchlist)
+      return this.storageService.set('library.watchlist', watchlist)
         .then(_ => this._watchList.next(watchlist))
     } else {
       throw `Movie with id=${movie.id} is already present in the watchlist`
     }
   }
 
-  removeFromWatchList(movieId: string) {
+  async removeFromWatchList(movieId: string) {
     const watchlist = this._watchList.getValue()
-    const movie = watchlist.find(m => m.id === movieId)
+    const movie = watchlist.find(m => m.id == movieId)
     if (movie) {
       watchlist.splice(watchlist.indexOf(movie), 1)
-      this.storageService.set('library.watchlist', watchlist)
+      return this.storageService.set('library.watchlist', watchlist)
         .then(_ => this._watchList.next(watchlist))
     } else {
       throw `Movie with id=${movie.id} is not present in the watchlist`
     }
   }
 
-  isInWatchList(movie: Movie): Observable<boolean> {
+  isInWatchList(movieId: string): Observable<boolean> {
     return this._watchList
       .asObservable()
       .pipe(
-        map(watchlist => watchlist.some(m => m.id === movie.id ))
+        map(watchlist => watchlist.some(m => m.id == movieId ))
       )
   }
 
-  addToLiked(movie: Movie) {
+  async addToLiked(movie: Movie) {
     const liked = this._liked.getValue()
-    if (!liked.find(m => m.id === movie.id)) {
+    if (!liked.find(m => m.id == movie.id)) {
+      const wasDisliked = this._disliked.getValue().some(m => m.id == movie.id)
+      if (wasDisliked) {
+        await this.removeFromDisliked(movie.id)
+      }
       liked.push(Movie.from(movie))
-      this.storageService.set('library.liked', liked)
+      return this.storageService.set('library.liked', liked)
         .then(_ => this._liked.next(liked))
+    } else {
+      throw `Movie with id=${movie.id} is already liked`
+    }
+  }
+
+  async addToDisliked(movie: Movie) {
+    const disliked = this._disliked.getValue()
+    if (!disliked.find(m => m.id === movie.id)) {
+      const wasLiked = this._liked.getValue().some(m => m.id == movie.id)
+      if (wasLiked) {
+        await this.removeFromLiked(movie.id)
+      }
+      disliked.push(Movie.from(movie))
+      return this.storageService.set('library.disliked', disliked)
+        .then(_ => this._disliked.next(disliked))
     } else {
       throw `Movie with id=${movie.id} is already liked`
     }
@@ -125,21 +146,41 @@ export class LibraryService {
 
   removeFromLiked(movieId: string) {
     const liked = this._liked.getValue()
-    const movie = liked.find(m => m.id === movieId)
+    const movie = liked.find(m => m.id == movieId)
     if (movie) {
       liked.splice(liked.indexOf(movie), 1)
-      this.storageService.set('library.liked', liked)
+      return this.storageService.set('library.liked', liked)
         .then(_ => this._liked.next(liked))
     } else {
       throw `Movie with id=${movie.id} is not yet liked`
     }
   }
 
-  isLiked(movie: Movie): Observable<boolean> {
+  removeFromDisliked(movieId: string) {
+    const disliked = this._disliked.getValue()
+    const movie = disliked.find(m => m.id == movieId)
+    if (movie) {
+      disliked.splice(disliked.indexOf(movie), 1)
+      return this.storageService.set('library.disliked', disliked)
+        .then(_ => this._disliked.next(disliked))
+    } else {
+      throw `Movie with id=${movie.id} is not yet disliked`
+    }
+  }
+
+  isLiked(movieId: string): Observable<boolean> {
     return this._liked
       .asObservable()
       .pipe(
-        map(liked => liked.some(m => m.id === movie.id))
+        map(liked => liked.some(m => m.id == movieId))
+      )
+  }
+
+  isDisliked(movieId: string): Observable<boolean> {
+    return this._disliked
+      .asObservable()
+      .pipe(
+        map(disliked => disliked.some(m => m.id == movieId))
       )
   }
 
@@ -150,5 +191,7 @@ export class LibraryService {
       .then(watchlist => this._watchList.next(watchlist || []))
     this.storageService.get("library.liked")
       .then(liked => this._liked.next(liked || []))
+    this.storageService.get("library.disliked")
+      .then(disliked => this._disliked.next(disliked || []))
   }
 }
